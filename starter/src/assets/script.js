0fffa6671f34b611230e197a2851d36f0f177eb0
Kamon West <kamon.west@gmail.com>
2024-09-30 22:35:57 -0400

    Bug fixes and updated functionality

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -38,23 +38,30 @@ const products = [
 */
 // TODO: Add Donut Peaches, add Dragonfruit, add Coconuts
 
-/* Declare an empty array named cart to hold the items in the cart */
-const cart = [];
-
+/* Empty array named cart to hold the items in the cart */
+let cart = [];
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
   - addProductToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
 function addProductToCart(productId) {
-  // Find the product by productId in the products array
-let product = products.find(product => product.productId === productId);
-// If product exists in the cart, increase the quantity
-product.quantity += 1;
+  // Finds the product by productId in the products array
+// If product exists in the cart, increases the quantity
+// let product = products.findIndex(product => product.productId === productId);
 
-if (!cart.includes(product)) {
+if (cart.find(item => item.productId === productId)) {
+let cartItem = cart.findIndex(item => item.productId === productId);
+cart[cartItem].quantity += 1;
+ console.log('product exists')
+} else {
+console.log('product')
+let product = products.find(product => product.productId === productId);
 
   cart.push(product);
+  let cartItem = cart.findIndex(item => item.productId === productId);
+  cart[cartItem].quantity += 1;
+ console.log('product does not exist')
 }
 }
 
@@ -64,10 +71,10 @@ if (!cart.includes(product)) {
 */
 
 function increaseQuantity(productId) {
-  // Find the product in the cart by productId
-  const product = products.find((product) => product.productId === productId);
-    // If the product is found, increase quantity
-    ++product.quantity;
+  // Finds the product in the cart by productId
+  const itemIndex = cart.findIndex((item) => item.productId === productId);
+    // If the product is found, increases quantity
+  cart[itemIndex].quantity++
   }
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
@@ -77,16 +84,15 @@ function increaseQuantity(productId) {
 */
 
 function decreaseQuantity(productId) {
-  // Find the product in the cart by productId
-  const product = products.find((product) => product.productId === productId);
-    // Decrease the product quantity
-    --product.quantity;
-
-    // If the quantity reaches 0, remove product from the cart
-    if (product.quantity === 0) {
-      removeProductFromCart(productId);
-    }  
-  }
+  // Finds the product in the cart by productId
+  const itemIndex = cart.findIndex((item) => item.productId === productId);
+    // Decreases the product quantity
+  cart[itemIndex].quantity--
+  
+    // If the quantity reaches 0, removes the product from the cart
+    if (cart[itemIndex].quantity < 0) {
+      removeProductFromCart(itemIndex);
+    }}
 
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
@@ -97,17 +103,14 @@ function decreaseQuantity(productId) {
 /** Function to remove product from the cart based on productId */
 function removeProductFromCart(productId) {
   // Filter out the product by productId from the cart
-  const index = cart.findIndex((product) => product.productId === productId);
-
+  const itemIndex = cart.findIndex((item) => item.productId === productId);
+// cart[itemIndex].quantity = 0;
   // If the product is found, update the quantity to 0
-  if (index !== 1) {
-
-    cart[index].quantity = 0;
-
-    cart.splice(index, 1);
+    console.log("before", cart)
+    cart.splice(itemIndex, 1);
+    console.log("after", cart)
 
   }
-}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total cost of all products
@@ -125,7 +128,7 @@ function cartTotal() {
 
   // Iterate through the cart and calculate the total cost
   for (let i = 0; i < cart.length; i++) {
-    totalCost += cart[i].price * cart[i].quantity;
+    totalCost += cart[i].quantity * cart[i].price;
   }
 
   // Return the total cost of all items in the cart
@@ -136,7 +139,6 @@ function cartTotal() {
 
 /** Function to empty all products from the cart */
 function emptyCart() {
-  // Set the cart array to empty
   cart = [];
 }
 
@@ -155,16 +157,24 @@ function pay(amount) {
   // Get the total amount of the products in the cart
   totalPaid += amount;
 
-  let change = totalPaid - cartTotal; // Calculate the change to be returned
+  let change = totalPaid - cartTotal(); // Calculate the change to be returned
 
+  // If the amount paid is less than the total, return the remaining balance
+  // if (amount < totalPaid) {
+  //  return totalPaid - amount;
+  // }
+  console.log("change", change)
+ if (change > -1) {
+  totalPaid = 0
+ }
   // If the amount paid is greater than the total, return the total amount due to customer
   return change;
 }
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
-/** Function to switch between currencies (USD, EUR, and YEN)
- * 
+/**
+ * @description Function to switch between currencies (USD, EUR, and YEN)
  * @param {number} amount - Amount of money to convert
  * @param {string} currency - Target currency to switch to ("USD", "EUR", "YEN")
  * @return {number} - Converted amount in chosen currency
@@ -176,9 +186,11 @@ function currency(amount, currency) {
     EUR: 0.90, // 1 USD = 0.90 EUR
     YEN: 140.3 // 1 USD = 140.3 YEN
   };
+  const baseCurrency = "USD";
+  const exchangeRate = rates[currency] / rates[baseCurrency];
 
   // Multiply the amount by the target currency exchange rate
-  return amount * (rates[currency] || 0);
+  return amount * exchangeRate;
 }
 
 /* The following is for running unit tests. 
@@ -187,7 +199,8 @@ function currency(amount, currency) {
    npm run test
 */
 
-module.exports = {
+module.exports = {  
+
    products,
    cart,
    addProductToCart,
@@ -197,6 +210,5 @@ module.exports = {
    cartTotal, 
    emptyCart,
    pay,
-   /* Uncomment the following line if completing the currency converter bonus */
-   currency,
+   currency
 }
